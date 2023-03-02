de0c693dd7c4287888ad3f29516b7589d5188750
Manjitha <manjitha.ee@gmail.com>
2023-03-02 19:48:49 +0100

    Commit Maskings

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import pandas as pd
 df = pd.read_csv('pokemon_data.csv')
 print(df.head(5))
 
+#Suppressing name column
 sup_df = df.drop('Name',axis="columns")
 print(sup_df.head(5))
 
+#Suppressing rows where Speed > 70
 sup_df = sup_df.drop(sup_df[sup_df.Speed > 70].index)
-print(sup_df.head())
+print(sup_df.head(5))
+
+#Masking Type 1
+sup_df['Type 1'] = 'XXXXX'
+print(sup_df.head(5))
+
+#Masking Type 2
+sup_df['Type 2'] = '*****'
+print(sup_df.head(5))
