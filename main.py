d4611ad543338c8ca67c68b4e81723db15beb2e3
Manjitha <manjitha.ee@gmail.com>
2023-03-06 17:00:49 +0100

    Commit final

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ from matplotlib import pyplot as plt
 df = pd.read_csv('pokemon_data.csv')
 print(df.head(5))
 
-#Suppressing name column
+## Suppressing name column
 sup_df = df.drop('Name',axis="columns")
 print(sup_df.head(5))
 
-#Suppressing rows where Speed > 70
+## Suppressing rows where Speed > 70
 sup_df = sup_df.drop(sup_df[sup_df.Speed > 70].index)
 print(sup_df.head(5))
 
-#Masking Type 1
+## Masking Type 1
 sup_df['Type 1'] = 'XXXXX'
 print(sup_df.head(5))
 
-#Masking Type 2
+## Masking Type 2
 sup_df['Type 2'] = '*****'
 print(sup_df.head(5))
 
-#Applying a function
 
-#Define function
+
+## Define function
 def doubler(x):
     return 2*x
 
+## Applying a function
 sup_df['Double Speed'] = sup_df['Speed'].apply(doubler)
 print(sup_df.head(5))
 
@@ -35,18 +36,18 @@ print(sup_df.head(10))
 #sup_df['Leg'] = sup_df['Legendary'].apply(lambda s:s[0]+'ABC'+s[s.find('S'):])
 #print(sup_df.head(5))
 
-##Plotting a histogram
+## Plotting a histogram
 sup_df['Double Speed'].hist(bins=140)
 plt.xlabel('Double Speed')
 plt.ylabel('Frequency')
 plt.title('Histogram for Double Speed')
 plt.show()
 
-##Generalization
+## Generalization
 sup_df['Double Speed'] = sup_df['Double Speed'].apply(lambda x: '>=80' if x>=80 else '<80')
 print(sup_df.head(10))
 
-##Bottom coding
+## Bottom coding
 sup_df['Trible Attack'].hist(bins=140)
 plt.xlabel('Trible Attack')
 plt.ylabel('Frequency')
@@ -61,7 +62,7 @@ plt.xlabel('Trible Attack')
 plt.ylabel('Frequency')
 plt.show()
 
-##Top Coding
+## Top Coding
 print(sup_df[sup_df['Trible Attack']>400])
 sup_df.loc[sup_df['Trible Attack'] > 400 , 'Trible Attack'] = 401
 print(sup_df[sup_df['Trible Attack']>400])
